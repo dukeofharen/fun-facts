49939e7412926ab38e60534ef9050e050387c11c
Duco Winterwerp <duco@winterwerp.nl>
2017-06-21 00:41:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 5000
+var facts = require('./facts')
+var favourites = []
+
+app.get('/facts/random', function (req, res) {
+    const index = Math.floor(Math.random()*facts.length);
+    const fact = facts[index];
+    res.send({ id: index, fact: fact })
+})
+
+app.get('/facts/favourite/:id', function(req, res){ 
+    const id = req.params.id
+    const fact = facts[id]
+    if(!fact){
+        res.sendStatus(404)
+    }
+    else{
+        favourites.push({ id: id, fact: fact })
+        res.sendStatus(200)
+    }
+})
+
+app.get('/facts/favourites', function(req, res){
+    res.send(favourites)
+})
+
+app.listen(port)
+console.log(`Listening on port ${port}`)
